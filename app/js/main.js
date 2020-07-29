9410ee11f5e094826e0f2e97d651d33365df09a4
Pjery3 <mixxer1265755@gmail.com>
2020-07-30 02:01:43 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
-$(function(){
-    
+$(function () {
+
+  $('.rate-star').rateYo({
+    halfStar: true,
+    starWidth: "13px",
+    rating: 5,
+    readOnly: true,
+  });
+
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  
+  })
+
+    var mixer = mixitup('.products__inner-box');
+
+
 })
